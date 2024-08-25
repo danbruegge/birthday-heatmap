7f693e6395ead28e8a2788004d3890c001a93bec
Daniel Brüggemann <gitkraken@barfooz.com>
2024-08-25 13:47:17 +0200

    Setup birthday heatmap

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,129 @@
-import Image from "next/image";
+"use client"
+
+import { PEOPLE } from "@/app/data";
+import { MONTHS } from "@/app/static-date-data";
+import { useRef, useState } from "react";
+
+type Person = {
+  name: string;
+  birthday: string;
+};
+
+function parseCSV(data: string): Person[] {
+  return data.split("\n").map((line) => {
+    const [name, birthday] = line.replace("\r", "").split(";");
+    return { name, birthday };
+  });
+}
+
+function parseVCARD(data: string): Person[] {
+  return data.split("BEGIN:VCARD").slice(1).map((vcard) => {
+    const name = vcard.match(/FN:(.*)/)?.[1];
+    const birthday = vcard.match(/BDAY:(.*)/)?.[1];
+    return name && birthday ? { name, birthday } : null;
+  }).filter((item) => item !== null);
+}
 
 export default function Home() {
+  const fileRef = useRef<HTMLInputElement>(null);
+  const [fileData, setFileData] = useState<Person[] | null>(null);
+
+  function handleReset() {
+    setFileData(null);
+
+    if (fileRef.current) {
+      fileRef.current.value
+    }
+  }
+
+  let daysOfMonth = MONTHS.map((month) => {
+    return [...Array(month.days)].map(() => 0);
+  });
+
+  (fileData ?? PEOPLE).forEach((item) => {
+    const [_, month, day] = item.birthday.split("-");
+    const m = parseInt(month, 10);
+    const d = parseInt(day, 10);
+
+    daysOfMonth[m - 1][d - 1] += 1;
+  })
+
+  const monthWeightList = MONTHS.map((_, month) => daysOfMonth[month].reduce((acc, curr) => acc + curr, 0))
+  const maxMonthWeight = Math.max(...monthWeightList);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex flex-col gap-12">
+      <h1 className="text-3xl underline">Birthday Heatmap for Friends</h1>
+      <div className="flex flex-col gap-12">
+        <form className="flex gap-4">
+          <label
+            htmlFor="file_input"
+            className="cursor-pointer px-4 py-2 leading-tight font-medium rounded-md text-black hover:bg-white bg-neutral-300 active:bg-neutral-500 select-none"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Upload csv/vCard File
+          </label>
+          <input
+            className="hidden"
+            ref={fileRef}
+            type="file"
+            id="file_input"
+            accept=".csv,.vcf"
+            onChange={async (e) => {
+              if (e?.target?.files?.length !== 0 && e?.target?.files?.[0]) {
+                const type = e?.target?.files?.[0].type
+                const reader = new FileReader();
+
+                reader.onload = async (file) => {
+                  if (typeof file.target?.result === "string") {
+                    if (type === "text/vcard") {
+                      setFileData(parseVCARD(file.target?.result));
+                    } else if (type === "text/csv") {
+                      setFileData(parseCSV(file.target?.result));
+                    }
+                  }
+                };
+                reader.readAsText(e?.target?.files?.[0]);
+              }
+            }}
+          />
+          <button
+            className="cursor-pointer px-4 py-2 leading-tight font-medium rounded-md text-black hover:bg-white bg-neutral-300 active:bg-neutral-500 select-none"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+        </form>
+        <div className="flex gap-8 sm:gap-4 flex-wrap">
+          {MONTHS.map((monthData, month) => {
+            let monthWeight = Math.min(Math.round(monthWeightList[month] / maxMonthWeight * 100) - 20);
+            monthWeight = Math.round(monthWeight / 10) * 10;
+
+            return (
+              <div key={monthData.name} className="w-auto sm:w-48 flex flex-col gap-2">
+                <div className="flex items-center gap-2">
+                  <div className="w-6 h-6 rounded" style={{
+                    backgroundColor: `hsl(73, 60%, ${monthWeight}%)`,
+                    border: `1px solid hsl(73, 10%, 20%)`,
+                  }}></div>
+                  <h2 className="w-24 pr-2 opacity-80 text-xl">{monthData.name}</h2>
+                </div>
+                <div className="flex flex-wrap gap-1">
+                  {daysOfMonth[month].map((dayWeight, day) => {
+                    const weight = dayWeight === 0 ? 0 : dayWeight + 1.5
+                    const light = weight * 10
+                    const lightValue = light > 80 ? 80 : light
+                    return (
+                      <div key={day} className={`w-6 h-6 flex items-center justify-center rounded`} style={{
+                        backgroundColor: dayWeight === 0 ? "hsl(73, 15%, 10%)" : `hsl(73, 60%, ${lightValue}%)`,
+                        border: `1px solid hsl(73, 10%, 20%)`
+                      }}></div>
+                    )
+                  })}
+                </div>
+              </div>
+            )
+          })}
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,17 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: [
+    'bg-emerald-100',
+    'bg-emerald-200',
+    'bg-emerald-300',
+    'bg-emerald-400',
+    'bg-emerald-500',
+    'bg-emerald-600',
+    'bg-emerald-700',
+    'bg-emerald-800',
+    'bg-emerald-900',
+  ],
   theme: {
     extend: {
       backgroundImage: {
